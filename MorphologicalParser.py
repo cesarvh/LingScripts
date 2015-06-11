349bba49b61c615fcae31d2e10aba605987274ba
cesarvh <cesarv.h@berkeley.edu>
2015-06-11 10:27:04 -0700

    fixed MorphologiclParser.py

--- MorphologicalParser.py
+++ MorphologicalParser.py
@@ -5,7 +5,12 @@ import csv
 	@Author Cesar Villalobos
 """
 
+
 def PatternFinder():
+	""" Given a file, and provided a "pattern", it finds words with this pattern and
+		returns their phonological environments
+	""" 
+
 	filename = input("What file would you like to use? ")
 	f = open(filename ,"r")
 	pattern_String = input("How many patterns would you like to find? Separate with spaces. ")
@@ -17,7 +22,7 @@ def PatternFinder():
 
 	for p in pattern_List:
 		patterns = []
-		print ("### Found patterns for " + p + " ###")
+		print ("### Patterns for " + p + " ###")
 		for word in word_list:
 			if p in word:
 				placeholder = word.find(p)
